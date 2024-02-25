22aaab05663c8a0d0b485dca0ec28cb2f1b01186
Sebastian Curtis T. Lavarias <sebastiancurtislavarias@gmail.com>
2024-02-25 22:37:32 +0800

    Save progress

---
+++ web-client/app/github/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+    return (
+        <div className="space-y-6">
+            <div>
+                <h3 className="text-2xl font-bold">Github callback</h3>
+                <p className="text-sm text-muted-foreground">
+                    github callback heres
+                </p>
+            </div>
+        </div>
+    );
+}
